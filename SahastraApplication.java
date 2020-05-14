5e78618b8b4bb297c5a868db8efa016b458fce71
priya-tripathi <63765397+priya-tripathi@users.noreply.github.com>
2020-05-14 09:33:30 +0530

    Add files via upload

---
+++ SahastraApplication.java
@@ -0,0 +1,15 @@
+package com.sg.packaging.Sahastra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SahastraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SahastraApplication.class, args);
+		System.out.println("hello world");
+		
+	}
+
+}
